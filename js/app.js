5272983ebe1a9cb9e82949bd84fe97244738d0d7
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-04 17:08:20 -0800

    add Video Toggle Button

--- js/app.js
+++ js/app.js
@@ -5,5 +5,16 @@ $(document).ready(function () {
     $('.navBtn').click(function(){
         $('.nav').toggleClass('nav--show')
     })
+    // video Switch
+    $('.video__switch-container').click(function(){
+        const value = $('.video__switch-btn').hasClass('btnSlide')
+        if(value){
+            $('.video__switch-btn').removeClass('btnSlide')
+            $('#video').get(0).play()
+        }else{
+            $('.video__switch-btn').addClass('btnSlide')
+            $('#video').get(0).pause()
+        }
+    })
 
 })
